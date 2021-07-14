98a48b82d478fd22fe140c2206f09f228c981627
shahzad khan <shahzadkhan311666@gmail.com>
2021-07-14 00:30:28 -0700

    this simple react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React ,{Component} from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Plan from './Plan.js'
+class App extends  Component{
+  state = {
+    items : [],
+    Text : ""
+  }
+  handlechange = e => {
+    this.setState({Text : e.target.value})
+  }
+  handleAdd = e =>{
+    if(this.state.Text !== ""){
+      const items=[this.state.items , this.state.Text];
+      this.setState({ items : items, Text: ""})
+    } 
+  }
+  render(){
+    return( 
+      <div>
+        <div className="container-fluid my-5">
+        <div className="row">
+        <div className="col-md-6 mx-auto text-white shadow-lg p-1" >
+          <h2 className="text-center ">ToDay's Plain</h2>
+          <div className="row">
+            <div className="col-9">
+              <input type="text" className="form-control" placeholder="enter yout plain" 
+                    value={this.state.Text} onChange={this.handlechange}/>
+            </div>
+            <div className="col-3">
+                    <button className="btn btn-success px-3 font-weight-bold"
+                      onClick={this.handleAdd}>ADD</button>
+            </div>
+            <div className="container-fluid my-5">
+              <ul className="list-unstyled row  ">
+             {
+               this.state.items.map((value, i)=> {
+                 return  <Plan  value={value}/>;
+               })
+             }
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/Plan.js
@@ -0,0 +1,7 @@
+//import React from "react";
+function Plan(props){
+    return <>
+      <li className="shadow my-2 ">{props.value}</li>
+    </>
+}
+export default Plan;
